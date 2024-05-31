f9a3447041c91562cd21901d571b27cd5459a95e
YSASM <1613921123@qq.com>
2024-05-31 14:57:22 +0800

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,262 @@
+﻿namespace avif_zip
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            listView1 = new ListView();
+            label1 = new Label();
+            bianmasuduinput = new TextBox();
+            modechoose = new ComboBox();
+            bianmasudu = new Label();
+            crf = new Label();
+            crfinput = new TextBox();
+            savepath = new Label();
+            savepathinput = new TextBox();
+            choosesavepath = new Button();
+            ffmpeg = new Label();
+            ffmpeginput = new TextBox();
+            ffmpegchoose = new Button();
+            startbtn = new Button();
+            resbox = new TextBox();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            SuspendLayout();
+            // 
+            // listView1
+            // 
+            listView1.AllowDrop = true;
+            listView1.Location = new Point(12, 30);
+            listView1.Name = "listView1";
+            listView1.Size = new Size(285, 408);
+            listView1.TabIndex = 0;
+            listView1.UseCompatibleStateImageBehavior = false;
+            listView1.View = View.List;
+            listView1.SelectedIndexChanged += listView1_SelectedIndexChanged;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(43, 17);
+            label1.TabIndex = 1;
+            label1.Text = "label1";
+            // 
+            // bianmasuduinput
+            // 
+            bianmasuduinput.Location = new Point(398, 61);
+            bianmasuduinput.Name = "bianmasuduinput";
+            bianmasuduinput.Size = new Size(380, 23);
+            bianmasuduinput.TabIndex = 2;
+            // 
+            // modechoose
+            // 
+            modechoose.FormattingEnabled = true;
+            modechoose.Location = new Point(322, 30);
+            modechoose.Name = "modechoose";
+            modechoose.Size = new Size(456, 25);
+            modechoose.TabIndex = 3;
+            // 
+            // bianmasudu
+            // 
+            bianmasudu.AutoEllipsis = true;
+            bianmasudu.AutoSize = true;
+            bianmasudu.Location = new Point(322, 67);
+            bianmasudu.Name = "bianmasudu";
+            bianmasudu.Size = new Size(56, 17);
+            bianmasudu.TabIndex = 4;
+            bianmasudu.Text = "编码速度";
+            // 
+            // crf
+            // 
+            crf.AutoSize = true;
+            crf.Location = new Point(322, 110);
+            crf.Name = "crf";
+            crf.Size = new Size(30, 17);
+            crf.TabIndex = 5;
+            crf.Text = "CRF";
+            crf.Click += crf_Click;
+            // 
+            // crfinput
+            // 
+            crfinput.Location = new Point(398, 107);
+            crfinput.Name = "crfinput";
+            crfinput.Size = new Size(380, 23);
+            crfinput.TabIndex = 6;
+            // 
+            // savepath
+            // 
+            savepath.AutoSize = true;
+            savepath.Location = new Point(322, 156);
+            savepath.Name = "savepath";
+            savepath.Size = new Size(56, 17);
+            savepath.TabIndex = 7;
+            savepath.Text = "保存路径";
+            savepath.Click += label2_Click;
+            // 
+            // savepathinput
+            // 
+            savepathinput.Location = new Point(398, 153);
+            savepathinput.Name = "savepathinput";
+            savepathinput.Size = new Size(301, 23);
+            savepathinput.TabIndex = 8;
+            // 
+            // choosesavepath
+            // 
+            choosesavepath.Location = new Point(703, 153);
+            choosesavepath.Name = "choosesavepath";
+            choosesavepath.Size = new Size(75, 23);
+            choosesavepath.TabIndex = 9;
+            choosesavepath.Text = "浏览";
+            choosesavepath.UseVisualStyleBackColor = true;
+            choosesavepath.Click += ChooseSavePath;
+            // 
+            // ffmpeg
+            // 
+            ffmpeg.AutoSize = true;
+            ffmpeg.Location = new Point(322, 188);
+            ffmpeg.Name = "ffmpeg";
+            ffmpeg.Size = new Size(74, 17);
+            ffmpeg.TabIndex = 10;
+            ffmpeg.Text = "ffmpeg路径";
+            // 
+            // ffmpeginput
+            // 
+            ffmpeginput.Location = new Point(398, 182);
+            ffmpeginput.Name = "ffmpeginput";
+            ffmpeginput.Size = new Size(301, 23);
+            ffmpeginput.TabIndex = 11;
+            // 
+            // ffmpegchoose
+            // 
+            ffmpegchoose.Location = new Point(703, 182);
+            ffmpegchoose.Name = "ffmpegchoose";
+            ffmpegchoose.Size = new Size(75, 23);
+            ffmpegchoose.TabIndex = 12;
+            ffmpegchoose.Text = "浏览";
+            ffmpegchoose.UseVisualStyleBackColor = true;
+            ffmpegchoose.Click += ChooseFfmpegPath;
+            // 
+            // startbtn
+            // 
+            startbtn.Location = new Point(322, 211);
+            startbtn.Name = "startbtn";
+            startbtn.Size = new Size(451, 24);
+            startbtn.TabIndex = 13;
+            startbtn.Text = "开始";
+            startbtn.UseVisualStyleBackColor = true;
+            startbtn.Click += Start;
+            // 
+            // resbox
+            // 
+            resbox.Location = new Point(318, 258);
+            resbox.Multiline = true;
+            resbox.Name = "resbox";
+            resbox.Size = new Size(454, 180);
+            resbox.TabIndex = 0;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(318, 238);
+            label2.Name = "label2";
+            label2.Size = new Size(56, 17);
+            label2.TabIndex = 14;
+            label2.Text = "运行结果";
+            label2.Click += label2_Click_2;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(398, 87);
+            label3.Name = "label3";
+            label3.Size = new Size(203, 17);
+            label3.TabIndex = 15;
+            label3.Text = "(数字0-8)，值越小速度越慢画质越高";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(398, 133);
+            label4.Name = "label4";
+            label4.Size = new Size(310, 17);
+            label4.TabIndex = 16;
+            label4.Text = "(数字0-63)，0为无损，1到63数字越小文件越大画质越高";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 17F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(resbox);
+            Controls.Add(startbtn);
+            Controls.Add(ffmpegchoose);
+            Controls.Add(ffmpeginput);
+            Controls.Add(ffmpeg);
+            Controls.Add(choosesavepath);
+            Controls.Add(savepathinput);
+            Controls.Add(savepath);
+            Controls.Add(crfinput);
+            Controls.Add(crf);
+            Controls.Add(bianmasudu);
+            Controls.Add(modechoose);
+            Controls.Add(bianmasuduinput);
+            Controls.Add(label1);
+            Controls.Add(listView1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private ListView listView1;
+        private Label label1;
+        private TextBox bianmasuduinput;
+        private ComboBox modechoose;
+        private Label bianmasudu;
+        private Label crf;
+        private TextBox crfinput;
+        private Label savepath;
+        private TextBox savepathinput;
+        private Button choosesavepath;
+        private Label ffmpeg;
+        private TextBox ffmpeginput;
+        private Button ffmpegchoose;
+        private Button startbtn;
+        private TextBox resbox;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,265 @@
+using Microsoft.VisualBasic.ApplicationServices;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.Intrinsics.X86;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace avif_zip
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.Text = "avifZip";
+            listView1.DragDrop += ListView1_DragDrop;
+            listView1.DragEnter += ListView1_DragEnter;
+            listView1.MouseUp += ListView1_MouseUp;
+            label1.Text = "将图片或包含图片的文件夹拖拽至下方列表（右键可删除）";
+            modechoose.Items.Add("保留精度");
+            modechoose.Items.Add("极限压缩");
+            modechoose.SelectedIndex = 0;
+            bianmasuduinput.Text = "0";
+            crfinput.Text = "0";
+            ffmpeginput.Text = System.Environment.CurrentDirectory + "\\ffmpeg.exe";
+            savepathinput.Text = System.Environment.CurrentDirectory + "\\output";
+        }
+
+        private void ListView1_MouseUp(object? sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                if (listView1.SelectedItems.Count > 0)
+                {
+                    listView1.Items.Remove(listView1.SelectedItems[0]);
+                }
+            }
+        }
+
+        private void ListView1_DragEnter(object? sender, DragEventArgs e)
+        {
+            if (e.Data != null && e.Data.GetDataPresent(DataFormats.FileDrop))
+                e.Effect = DragDropEffects.All;
+            //重要代码：表明是所有类型的数据，比如文件路径
+            else
+                e.Effect = DragDropEffects.None;
+        }
+        private string[] acceptType = ["png", "jpeg", "jpg"];
+
+        private bool ExistImage(string path)
+        {
+            System.Collections.IList list = listView1.Items;
+            for (int i = 0; i < list.Count; i++)
+            {
+                ListViewItem item = (ListViewItem)list[i];
+                if ((string)item.Tag == path)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+
+
+        private void AddImage(string path)
+        {
+            var temp = path.Replace("\\", "/").Split("/");
+            var fileName = temp[temp.Length - 1];
+            temp = fileName.Split(".");
+            var type = temp[temp.Length - 1];
+            if (!acceptType.Contains(type))
+            {
+                return;
+            }
+            if (ExistImage(path))
+            {
+                return;
+            }
+            ListViewItem item = new ListViewItem(path);
+            item.Tag = path;
+            item.Text = fileName;
+            listView1.Items.Add(item);
+        }
+
+        private void ListView1_DragDrop(object? sender, DragEventArgs e)
+        {
+            if (e.Data != null)
+            {
+                string[] f = (string[])e.Data.GetData(DataFormats.FileDrop);
+                for (int i = 0; i < f.Length; i++)
+                {
+                    if (File.Exists(f[i]))
+                    {
+                        AddImage(f[i]);
+                    }
+                    else if (Directory.Exists(f[i]))
+                    {
+                        try
+                        {
+                            string[] files = Directory.GetFiles(f[i], "*", SearchOption.AllDirectories);
+                            foreach (string file in files)
+                            {
+                                AddImage(file);
+                            }
+                        }
+                        catch { }
+                    }
+                    else
+                    {
+                        // 都不是
+
+                    }
+                }
+            }
+
+
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void crf_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ChooseSavePath(object sender, EventArgs e)
+        {
+            using (FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog())
+            {
+                folderBrowserDialog.Description = "请选择一个文件夹";
+                DialogResult dialogResult = folderBrowserDialog.ShowDialog();
+
+                if (dialogResult == DialogResult.OK)
+                {
+                    string selectedFolderPath = folderBrowserDialog.SelectedPath;
+                    savepathinput.Text = selectedFolderPath;
+                }
+            }
+        }
+
+        private void ChooseFfmpegPath(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Title = "选择文件";
+                openFileDialog.Filter = "所有文件 (*.*)|*.*";
+                openFileDialog.FilterIndex = 1;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    // 用户选择了文件
+                    ffmpeginput.Text = openFileDialog.FileName;
+                }
+            }
+        }
+
+        private void RunCMDCommand(string command)
+        {
+            resbox.Text = "";
+            ProcessStartInfo startInfo = new ProcessStartInfo("cmd.exe")
+            {
+                RedirectStandardInput = true,
+                RedirectStandardOutput = true,
+                UseShellExecute = false,
+                CreateNoWindow = false,
+            };
+
+            Process cmdProcess = new Process()
+            {
+                StartInfo = startInfo
+            };
+
+            cmdProcess.Start();
+            cmdProcess.StandardInput.WriteLine(command);
+            cmdProcess.StandardInput.WriteLine("exit");
+
+            cmdProcess.BeginOutputReadLine();
+            cmdProcess.OutputDataReceived += CmdProcess_OutputDataReceived;
+        }
+        private void CmdProcess_OutputDataReceived(object sender, DataReceivedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(e.Data))
+            {
+                // 在这里处理实时接收到的输出
+                // 例如，更新UI线程中的文本框来显示结果
+                this.Invoke(new Action(() =>
+                {
+                    resbox.AppendText(e.Data + Environment.NewLine);
+                }));
+            }
+        }
+
+        private void Start(object sender, EventArgs e)
+        {
+            var mode = "";
+            if (modechoose.SelectedIndex == 1)
+            {
+                mode = "-pix_fmt yuv444p";
+            }
+            if (ffmpeginput.Text == "")
+            {
+                MessageBox.Show("请填写ffmpeg.exe路径", "错误！", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            if (savepathinput.Text == "")
+            {
+                MessageBox.Show("请填写保存路径", "错误！", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            if (bianmasuduinput.Text == "")
+            {
+                bianmasuduinput.Text = "0";
+            }
+            if (crfinput.Text == "")
+            {
+                crfinput.Text = "0";
+            }
+            var cmd = "\"" + ffmpeginput.Text + "\" -i \"{path}\" -c:v libaom-av1 " + mode + " -cpu-used " + bianmasuduinput.Text + " -crf " + crfinput.Text + " \"" + savepathinput.Text + "\\{filename}.avif\"";
+            System.Collections.IList list = listView1.Items;
+            if (listView1.Items.Count <= 0)
+            {
+                MessageBox.Show("没有选择任何文件", "错误！", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            if (!Directory.Exists(savepathinput.Text))
+            {
+                Directory.CreateDirectory(savepathinput.Text);
+            }
+            for (int i = 0; i < list.Count; i++)
+            {
+                ListViewItem item = (ListViewItem)list[i];
+                var path = (string)item.Tag;
+                var fileName = (string)item.Text.Split(".")[0];
+                if (File.Exists(savepathinput.Text + "\\" + fileName + ".avif"))
+                {
+                    File.Delete(savepathinput.Text + "\\" + fileName + ".avif");
+                }
+                var runCmd = cmd.Replace("{path}", path);
+                runCmd = runCmd.Replace("{filename}", fileName);
+                RunCMDCommand(runCmd);
+                listView1.Items.Clear();
+            }
+        }
+
+        private void label2_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click_2(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
